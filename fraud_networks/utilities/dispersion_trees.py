9a70ae8f9ae1eb7cdd79618cd28a2840c6f9472a
Thunder <thunder@uoregon.edu>
2016-09-11 17:40:17 +0100

    Remove unnecessary inheritance and attribute

--- fraud_networks/utilities/dispersion_trees.py
+++ fraud_networks/utilities/dispersion_trees.py
@@ -6,17 +6,16 @@ class DispersionEdges(object):
     def __init__(self, fraud_case):
         self.edge_dataframe = pd.DataFrame(fraud_case)
 
-class DispersionTree(nx.DiGraph): #TODO: Should this be inheriting from nx.DiGraph?
+class DispersionTree: #TODO: Should this be inheriting from nx.DiGraph?
 
     def __init__(self, fraud_case=None, **kwds): # Should I put the actual macro parameters instead of kwargs?
         """Takes in a fraud instance and returns a dispersion arborescence tree."""
 
         if fraud_case == None:
-            self.example = fraud_case=test_data.fraud_case01()
-        self.graph = nx.from_pandas_dataframe(df=fraud_case, source='source', target='target', create_using=nx.DiGraph())
-        self.edge_dataframe = nx.to_pandas_dataframe(self.graph)
-
-        #self.edge_dataframe = DispersionEdges(fraud_case).edge_dataframe #TODO: Should DispersionTree take DispersionEdges as input rather than fraud_case?
-        #self = nx.from_pandas_dataframe(self.edge_dataframe, source='source', target='target') 
+            fraud_case = test_data.fraud_case01()
         
+
+        self.graph = nx.from_pandas_dataframe(df=fraud_case, source='source', target='target', create_using=nx.DiGraph())
+
+        self.edge_dataframe = nx.to_pandas_dataframe(self.graph)
         
