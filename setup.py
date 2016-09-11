141f2b075e56a6eea7e254018fabee8b1e308ffb
Thunder <thunder@uoregon.edu>
2016-09-11 14:46:16 +0100

    add setup.py to package fraud_networks

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dispersion_trees',
+      version='0.1',
+      description='',
+      url='',
+      author='',
+      author_email='',
+      #license='MIT',
+      packages=['dispersion_trees'],
+      zip_safe=False)
