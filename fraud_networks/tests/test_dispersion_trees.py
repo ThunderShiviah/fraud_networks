a7bb401d66d8152f516b0a4cceb6cc77d2dda942
Thunder <thunder@uoregon.edu>
2016-09-11 14:49:05 +0100

    Fix imports. Utilize DispersionTree default behavior. Add check to validate tree.

--- fraud_networks/tests/test_dispersion_trees.py
+++ fraud_networks/tests/test_dispersion_trees.py
@@ -1,14 +1,18 @@
-from utilities.dispersion_trees import DispersionTree, DispersionEdges
-from utilities import test_data
+from fraud_networks import DispersionTree, DispersionEdges
+
+#from utilities.dispersion_trees import DispersionTree, DispersionEdges
+from fraud_networks.utilities import test_data
+import networkx as nx
 
 def test_DispersionEdges():
     disp_edges = DispersionEdges(test_data.fraud_case01())
     assert not disp_edges.edge_dataframe.empty
 
 def test_DispersionTree():
-    disp_tree = DispersionTree(test_data.fraud_case01())
+    disp_tree = DispersionTree()
     assert not disp_tree.edge_dataframe.empty
     assert disp_tree.graph
+    assert nx.is_tree(disp_tree)
 
 
 
